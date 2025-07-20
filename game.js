e4092e8f3658c46393ce12e48e123830104f7da1
valartan <anthony.maria@live.fr>
2025-07-20 16:12:15 +0200

    Update game.js

--- game.js
+++ game.js
@@ -220,9 +220,9 @@ function displayLeaderboard() {
 
   function resetGame() {
 // 🔽 Adapter les vitesses selon le device
-  player.gravityDown = isMobile ? 0.6 : 0.9;
-  player.gravityUp = isMobile ? -0.6 : -0.8;
-  player.maxSpeed = isMobile ? 4 : 6;
+  player.gravityDown = isMobile ? 0.9 : 0.9;
+  player.gravityUp = isMobile ? -1.2 : -0.8;
+  player.maxSpeed = isMobile ? 7 : 6;
   player.radius = isMobile ? 18 : 25;
 
     bubbles = [];
